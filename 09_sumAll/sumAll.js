48a4255d753349b256a3da466a0f04d6d773bb28
Ethan Delgado <efd7152@nyu.edu>
2025-09-15 12:17:56 +1000

    complete ex 9 sumAll.js and sumAll.spec.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    let max = num1 > num2 ? num1 : num2;
+    let min = num1 > num2 ? num2 : num1;
 
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
