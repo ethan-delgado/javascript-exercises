5287dffb46062ccfec081880c6d5cca6c5686726
Ethan Delgado <efd7152@nyu.edu>
2025-09-15 12:24:38 +1000

    complete ex 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  fahrenheit = Math.round((fahrenheit - 32) * 5/9 * 10) / 10;
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  celsius = Math.round((celsius * 9/5 + 32) * 10) / 10;
+  return celsius;
 };
 
 // Do not edit below this line
