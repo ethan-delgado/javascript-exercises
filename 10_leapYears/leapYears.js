3b3fd36650ef69dd15bfb141ff41fda7f4644702
Ethan Delgado <efd7152@nyu.edu>
2025-09-15 12:21:22 +1000

    finished ex 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year !== 'number' || !Number.isInteger(year)) {
+        throw new Error('Input must be an integer.');
+    }
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true; // Divisible by 400, so it's a leap year
+            } else {
+                return false; // Divisible by 100 but not by 400, so not a leap year
+            }
+        } else {
+            return true; // Divisible by 4 but not by 100, so it's a leap year
+        }
+    } else {
+        return false; // Not divisible by 4, so not a leap year
+    }   
 };
 
 // Do not edit below this line
