f5220c841b73dac8e7b77f34b3b44947e38bf4f6
Ethan Delgado <efd7152@nyu.edu>
2025-09-17 09:04:22 +1000

    complete ex 12 of quick js exs

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(num1, num2) {
+  if (num1 < 0 && num2 < 0) {
+    return Math.abs(num2) - Math.abs(num1);
+  } else if (num1 < 0) {
+    return num2 + Math.abs(num1);
+  } else if (num2 < 0) {
+    return num1 + Math.abs(num2);
+  } else {
+    return num1 - num2;
+  }
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(num => {
+    product *= num;
+  });
+  return product;  
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let product = 1;
+  for (let i = num; i > 1; i--) {
+    product *= i;
+  }
+  return product;xw
 };
 
 // Do not edit below this line
